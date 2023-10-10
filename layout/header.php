2a46d3cdabffd45864079bb321da77b59edf2507
stek ferney aguirre ceballos <stek-04@hotmail.com>
2023-10-09 23:52:46 -0500

    INSTALANDO DATATABLE

--- layout/header.php
+++ layout/header.php
@@ -2,7 +2,7 @@
 <!-- HTML -->
   <html lang="es">
   <!-- HEAD -->
-    <head>
+  <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title><?php echo APP_NAME;?></title>
@@ -16,8 +16,14 @@
       <!-- Theme style -->
       <link rel="stylesheet" 
       href="<?php echo $URL;?>/public/templeates/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+      <!-- ICONOS DE BOOTSTRAP -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
       <!-- LIBRERIA DE SWEETALERT -->
       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+      <!-- DataTables -->
+      <link rel="stylesheet" href="<?php echo $URL;?>/public/templeates/AdminLTE-3.2.0/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+      <link rel="stylesheet" href="<?php echo $URL;?>/public/templeates/AdminLTE-3.2.0/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+      <link rel="stylesheet" href="<?php echo $URL;?>/public/templeates/AdminLTE-3.2.0/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     </head>
   <!-- ////HEAD -->
   <!-- BODY -->
