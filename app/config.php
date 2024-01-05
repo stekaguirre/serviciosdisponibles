96de03f195ede3ede02a7c7f8dde99512309dcd5
stek ferney aguirre ceballos <stek-04@hotmail.com>
2024-01-05 12:23:50 -0500

    LISTO MODULO USUARIOS Y ROLES

--- app/config.php
+++ app/config.php
@@ -1,27 +1,28 @@
+<!-- ARCHIVO DE CONFIGURACION -->
+
 <?php
 
-define ('APP_NAME','ServiciosDisponibles');
-define('SERVIDOR','localhost');
-define('USUARIO','root');
-define('PASSWORD','');
-define('BD','serviciosdisponibles');
+    define('APP_NAME','servicios disponibles');
+    define('SERVIDOR','localhost');
+    define('USUARIO','root');
+    define('PASSWORD','');
+    define('BD','serviciosdisponibles');
 
-$URL = "http://localhost/serviciosdisponibles";
+    $URL = "http://localhost/serviciosdisponibles";
+
+    date_default_timezone_set("America/Bogota");
+    $fechahora = date('Y-m-d H:i:s');
 
 
-date_default_timezone_set("America/Bogota");
-$fechahora = date('Y-m-d H:i:s');
+    $servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
 
-
-$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
-
-try{
-    $pdo = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
-   // echo "Conexión exitosa a la base de datos";
-}catch (PDOException $e){
-    //print_r($e);
-    echo "Error al conectar a la base de datos";
-}
-
+    try{
+        $pdo = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    // echo "Conexión exitosa a la base de datos ";
+    }catch (PDOException $e){
+        //print_r($e);
+        echo "Error al conectar a la base de datos";
+    }
 
 ?>
+
